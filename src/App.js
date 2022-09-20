895f2ba32d8b2756e2c6d298641ef7313fa8313d
David Rousseau <25441032+xrousseau@users.noreply.github.com>
2022-09-20 08:45:41 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [status, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 60000);
+
+    return () => clearInterval(interval);
+
+  }, [])
+
+  const fetchData = () => {
+    fetch("https://bridge-status.azurewebsites.net/api/GetBridgeStatus?id=M1")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setItems(result);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  };
+
+
+  if (error) {
+    return <div>Error : {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+
+      return (
+        <div style={{ width: '100vw', height: '100vh'  }}>
+          <CircularProgressbar value={status.openedMinutesRemaining} maxValue={60} text={`${status.openedMinutesRemaining} m`} />;
+        </div>
+      );
+  }
+
 }
 
 export default App;
